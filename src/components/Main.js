16a8956f055b947488a673cff877f20b82c6d97c
IlhamNur <romdhoninuril@gmail.com>
2025-02-23 14:53:28 +0700

    done

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,20 +33,21 @@ const Container = styled.div`
 const Contact = styled.a`
   color: ${(props) => props.theme.text};
   position: absolute;
-  top: 2rem;
-  right: calc(1rem + 2vw);
-  text-decoration: none;
-  z-index: 1;
-`;
-const BLOG = styled(NavLink)`
-  color: ${(props) => props.theme.text};
-  position: absolute;
   top: 50%;
   right: calc(1rem + 2vw);
   transform: rotate(90deg) translate(-50%, -50%);
   text-decoration: none;
   z-index: 1;
 `;
+// const BLOG = styled(NavLink)`
+//   color: ${(props) => props.theme.text};
+//   position: absolute;
+//   top: 50%;
+//   right: calc(1rem + 2vw);
+//   transform: rotate(90deg) translate(-50%, -50%);
+//   text-decoration: none;
+//   z-index: 1;
+// `;
 const WORK = styled(NavLink)`
   color: ${(props) => (props.click ? props.theme.body : props.theme.text)};
 
@@ -166,7 +167,7 @@ const Main = () => {
             Say hi..
           </motion.h2>
         </Contact>
-        <BLOG to="/blog">
+        {/* <BLOG to="/blog">
           <motion.h2
             initial={{
               y: -200,
@@ -181,7 +182,7 @@ const Main = () => {
           >
             Blog.
           </motion.h2>
-        </BLOG>
+        </BLOG> */}
         <WORK to="/work" click={+click}>
           <motion.h2
             initial={{
